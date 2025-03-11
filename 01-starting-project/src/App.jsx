3fc02cd21d82f930845b61352f69e9d5b490f68d
kjy735 <wodus3806@gmail.com>
2025-03-11 21:48:50 +0900

    feat: 20250311 react essential project started

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,16 +1,40 @@
+// import Header from "./components/Header";
+
+const reactDescriptionsKor = ['근본적인', '결정적인', '핵심적인'];
+const reactDescriptionsEng = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1)); // 얘의 return 값은 0~2의 int가 반환됨
+  // 어떻게? Math.random() * (max + 1) 얘의 범위가 0.xxxx ~ 2.xxxx이기 때문
+}
+
+function Header() {
+  const num = getRandomInt(2);
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React 필수 요소 복습</h1>
+      <p>
+        {reactDescriptionsEng[num]} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+
+      <br />
+      대부분의 앱을 위해 당신이 필요로 할 {reactDescriptionsKor[num]} React 개념 학습✏️
+    </header>   
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
+
+        <br />
+
+        <h2>시작해봅시다!</h2>
       </main>
     </div>
   );
