68b1238905bf30725234cb8690d77bb5c021110f
kjy735 <wodus3806@gmail.com>
2025-03-11 20:01:23 +0900

    feat: 20250311 react router app started

--- shppinglist/src/App2.tsx
+++ shppinglist/src/App2.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
-import './App.css'
 import  Container  from '@mui/material/Container';
 import  AppBar  from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import AddItem from './components/AddItem';
+// 쇼핑 항목을 표시하는 목록 관련 컴포넌트들을 import
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+import './App.css'
 
 export type Item = {
   product: string;
@@ -30,6 +35,20 @@ function App() {
         </Toolbar>
       </AppBar>
       <AddItem addItem={addItem}/>
+
+      <List>
+        {
+          items.map((item, index) => // 두번째 argument를 넣어야지만 오류가 생기지 않음
+            <ListItem key={index} divider>
+              <ListItemText 
+                primary={item.product}
+                secondary={item.amount}>
+                </ListItemText>
+            </ListItem>
+          )  
+        }
+      </List>
+
     </Container>
     </>
   )
