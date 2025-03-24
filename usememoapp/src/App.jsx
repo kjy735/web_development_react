c54a8b39ced6c6acc7da0005826fbc29e31f91f8
kjy735 <wodus3806@gmail.com>
2025-03-24 15:23:36 +0900

    feat : 20250321 reacttodoapp started

--- usememoapp/src/App.jsx
+++ usememoapp/src/App.jsx
@@ -1,8 +1,39 @@
+import { useMemo, useState, useEffect } from "react";
+import "./App.css";
+
 function App() {
+  const [ number, setNumber ] = useState(0);
+  const [ isTrue, setIsTrue ] = useState(true);
+
+  // const answer = isTrue ? "true" : "false"; 기초자료형(primitive type)
+
+  // const answer = { bool: isTrue ? "true" : "false"};
+  // 이대로면 객체 주소값이 렌더링 될때마다 바뀌게 된다
+  const answer = useMemo(() => {
+    return { bool : isTrue ? "true" : "false" }; 
+  }, [isTrue]);
+
+  // answer가 바뀔 때만 콘솔이 찍히도록 useEffect 적용
+  useEffect(() => {
+    console.log('answer 값이 변경되었습니다.');
+  }, [answer]); 
+  // []: 처음 렌더링할때만 실행하겠다는 뜻 / 특정 상태에서만 실행하고 싶다면 []안에 특정 상태를 넣어야함
+
   return (
-    <>
-    
-    </>
+    <div>
+      <p>number</p>
+      <input type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)} 
+      />
+      <hr />
+      <p>answer</p>
+      {/* <p>True or False? : {answer}</p> */}
+      {/* <p>True or False? : {answer.bool}</p> */}
+      <p>True or False? : {answer['bool']}</p>
+
+      <button onClick={() => setIsTrue(!isTrue)}>Reverse</button>
+    </div>
   );
 }
 
